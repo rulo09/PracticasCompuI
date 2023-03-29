c57aa87ce5bf02895da1242816f34f9625723841
rulo09 <ramanez2003@gmail.com>
2023-03-28 23:28:34 -0600

    Encontramos el pivote, hicimos una función de intercambio de filas, e iniciamos el primer paso de gaussjordan al eliminar la primera columna. Necesitamos hacer lo mismo para las columnas 2 y 3 y ver si la función se indetermina. Necesitamos saber cómo imprimir sólo la solución

--- GaussJordan/GaussJordan.cpp
+++ GaussJordan/GaussJordan.cpp
@@ -18,6 +18,12 @@ void GaussJordan(matriz & miMatriz);
 template <typename matriz>
 void ImprimirSolucion(matriz & miMatriz);
 
+template<typename matriz>
+int buscarPivote(matriz &miMatriz, int fila, int numFilas); // Nos permite localizar el número más grande y hacerlo elde la primera fila
+
+template<typename matriz>
+void intercambiarFilas(matriz &miMatriz, int fila1, int fila2, int columnas);
+
 
 int main()
 {
@@ -36,6 +42,11 @@ int main()
 
     // Aplicamos el método de Gauss-Jordan sobre nuestra matriz
     GaussJordan(miMatriz);
+
+
+    buscarPivote(miMatriz);
+
+    intercambiarFilas(miMatriz);
 
     // Imprimimos la solución de la matriz
     ImprimirSolucion(miMatriz);
@@ -87,21 +98,63 @@ void ImprimirSolucion(matriz & miMatriz)
 
 }
 
+template <typename matriz>
+int buscarPivote(matriz &miMatriz, int fila, int numFilas){
+    int columna = fila;
+    int indicePivote = -1;
+
+    float max = miMatriz[fila][columna];
+    for(int i = fila; i<numFilas; i++) {
+        if(miMatriz[i][columna] != 0 & miMatriz[i][columna] >= max){
+            max = miMatriz[i][columna];
+            indicePivote = i;
+        }
+    }
+}
+
 /*
 Implementa el algoritmo de Gauss-Jordan sobre 'miMatriz', finalizando en ella la solución del algoritmo.
 No regresa ningún valor.
 */
+template<typename matriz>
+void intercambiarFilas(matriz &miMatriz, int fila1, int fila2, int cols){
+    for(int j = 0; j < cols; j++){
+        int temp = miMatriz[fila1][j];
+        miMatriz[fila1][j] = miMatriz[fila2][j];
+        miMatriz[fila2][j] = temp;
+    }
+}
+
 template <typename matriz>
 void GaussJordan(matriz & miMatriz){
-    int variables = miMatriz.size();
-    for (int i = 0; i < variables; i++) {
-        cout << "[ ";
-        for (int j = 0; j < variables + 1; j++)
-            cout << miMatriz[0][j] / miMatriz[0][0] << "\t";
-        cout << "]\n";
+    int filas =miMatriz.size(), columnas = miMatriz.size() +1;
+
+    for(int i = 0; i < filas; i ++){
+        int indicePivote = buscarPivote(miMatriz, i, filas);
+        if (indicePivote != -1){
+            intercambiarFilas((miMatriz, i, indicePivote, columnas);
+        }
+        else{
+            cout<< "El sistema no tiene solución" << endl;
+            exit(0);
+        }
+
+    }
+    for (int i = 0; i < filas; i++){
+        int valorPivote = miMatriz[0][0];
+        miMatriz[0][columnas] /= miMatriz[0][0];
+        if( i != 0) {
+            miMatriz[filas][columnas] = miMatriz[filas][columnas] - (miMatriz[filas][columnas] * valorPivote);
+
+            }
+        }
+
+
     }
 
-        }
+
+
+
 
 
 
