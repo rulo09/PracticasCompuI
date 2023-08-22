5cd5719b11b9c38c9eda9911de10cc9d1c81a960
rulo09 <ramanez2003@gmail.com>
2023-08-22 11:19:00 -0600

    Agregamos atributos y metodos a el producto

--- TIENDADEPARTMENTAL/Producto.h
+++ TIENDADEPARTMENTAL/Producto.h
@@ -4,10 +4,19 @@
 
 #ifndef TIENDADEPARTMENTAL_PRODUCTO_H
 #define TIENDADEPARTMENTAL_PRODUCTO_H
-
+#include <string>
+using namespace std;
 
 class Producto {
+private:
+    float precio;
+    string nombre;
+    int inventario;
+    int barras[4];
 
+public:
+    void setCompra(float precio, string nombre, int barras[]);
+    void setInventario(int inventario);
 };
 
 
